f0a3a29056147c5d9663a52c37f4212e0036047e
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-21 15:00:17 +0330

    createEntity

---
+++ FinanceBill/FinanceBill.Domain/Entities/Bill.cs
@@ -0,0 +1,11 @@
+﻿namespace FinanceBill.Domain.Entities;
+
+public class Bill
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public double Price { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? UpdatedAt { get; set; }
+}
