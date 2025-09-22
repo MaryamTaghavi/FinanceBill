d7e2567d3d7d669086a7f6494113e7fe6b5d58b9
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-22 10:14:35 +0330

    addDataAnnotations

--- FinanceBill/FinanceBill.Domain/Entities/Bill.cs
+++ FinanceBill/FinanceBill.Domain/Entities/Bill.cs
@@ -1,30 +1,39 @@
-﻿namespace FinanceBill.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceBill.Domain.Entities;
 
 public class Bill
 {
     /// <summary>
     /// شناسه
     /// </summary>
+    [Key]
     public int Id { get; set; }
 
     /// <summary>
     /// نام
     /// </summary>
+    [MaxLength(30)]
+    [Required]
     public string Name { get; set; }
 
     /// <summary>
     /// توضیحات
     /// </summary>
+    [MaxLength(30)]
     public string? Description { get; set; }
 
     /// <summary>
     /// مبلغ
     /// </summary>
+    [Required]
+    [Range(1000, 100000000, ErrorMessage = "باید بین 1000 و 100000000 باشد")]
     public double Price { get; set; }
 
     /// <summary>
     /// ایجاد شده در
     /// </summary>
+    [Required]
     public DateTime CreatedAt { get; set; }
 
     /// <summary>
